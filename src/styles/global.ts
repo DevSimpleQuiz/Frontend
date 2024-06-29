a3f50a70d9352a48f15e9735b72a84aa558a5c65
gahyeon <117976216+gahyeon11@users.noreply.github.com>
2024-06-29 13:52:39 +0900

    Update src/styles/global.ts
    feat: global 들여쓰기, 띄어쓰기 추가
    
    Co-authored-by: damii <58524208+Dami-LEE00@users.noreply.github.com>

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,22 +1,25 @@
 import "sanitize.css";
 import { createGlobalStyle } from "styled-components";
 
+import { createGlobalStyle } from "styled-components"
+
 export const GlobalStyle = createGlobalStyle`
-*{
-    margin : 0;
-    padding : 0;
+  * {
+    margin: 0;
+    padding: 0;
     box-sizing: border-box;
-}
-body{
-    margin : 0;
-    padding : 0;
-}
-a{
-    text-decoratiom: none;
-}
-ul, li{
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul, li {
     list-style: none;
-}
-
-
+  }
 `;
