c980b033be3451b66932b9b56bb31ec0b9b600b4
gahyeon <117976216+gahyeon11@users.noreply.github.com>
2024-07-02 20:00:41 +0900

    feat: 배너 이동 버튼 기능 추가

--- src/components/common/banner/Banner.tsx
+++ src/components/common/banner/Banner.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination } from "swiper/modules";
+import { Autoplay, Pagination, Navigation } from "swiper/modules"; // Navigation 모듈 추가
 import "swiper/css";
+import "swiper/css/navigation";
 
 import Banner1 from "../../../assets/bannerImg/Banner1.svg";
 import Banner2 from "../../../assets/bannerImg/Banner2.svg";
@@ -11,21 +12,29 @@ import Banner4 from "../../../assets/bannerImg/Banner4.svg";
 import Banner5 from "../../../assets/bannerImg/Banner5.svg";
 import Banner6 from "../../../assets/bannerImg/Banner6.svg";
 import Banner7 from "../../../assets/bannerImg/Banner7.svg";
+import Banner8 from "../../../assets/bannerImg/Banner8.svg";
 
-const bannerImages = [Banner1, Banner2, Banner3, Banner4, Banner5, Banner6, Banner7 ];
+const bannerImages = [
+  Banner8,
+  Banner2,
+  Banner3,
+  Banner5,
+  Banner4,
+  Banner6,
+  Banner1,
+  Banner7,
+];
 
 function Banner() {
   return (
     <BannerStyle>
       <Swiper
-        modules={[Autoplay, Pagination]} 
-        // spaceBetween={50} 
-        // slidesPerView={1.5} 
-        spaceBetween={0} 
-        slidesPerView={1} 
-        centeredSlides={true} 
-        loop={true} 
-        navigation
+        modules={[Autoplay, Pagination, Navigation]}
+        spaceBetween={50}
+        slidesPerView={1.35}
+        centeredSlides={true}
+        loop={true}
+        loopFillGroupWithBlank= {true}
         autoplay={{
           delay: 4500,
           disableOnInteraction: false,
@@ -34,8 +43,12 @@ function Banner() {
         pagination={{
           clickable: true,
         }}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        }}
       >
-        {/* 각 이미지를 SwiperSlide로 렌더링합니다. */}
+        <div className="swiper-button-prev"></div>
         {bannerImages.map((image, index) => (
           <SwiperSlide key={index}>
             <SlideContainer>
@@ -45,6 +58,7 @@ function Banner() {
             </SlideContainer>
           </SwiperSlide>
         ))}
+        <div className="swiper-button-next"></div>
       </Swiper>
     </BannerStyle>
   );
@@ -52,6 +66,20 @@ function Banner() {
 
 const BannerStyle = styled.div`
   padding-top: 50px;
+  position: relative;
+
+  .swiper-button-prev,
+  .swiper-button-next {
+    color: #000;
+    background-color: rgba(255, 255, 255, 0.5);
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: -1% 13%;
+  }
 `;
 
 const SlideContainer = styled.div`
@@ -71,8 +99,7 @@ const ImageContainer = styled.div`
 `;
 
 const Image = styled.img`
-  width: 1200px;
-  /* max-width: 100%; */
+  max-width: 100%;
   max-height: 100%;
   object-fit: contain;
 `;
