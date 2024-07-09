0fa01b25c485eea27464c0a8a1ffafe9349098f2
gahyeon <117976216+gahyeon11@users.noreply.github.com>
2024-07-09 22:34:51 +0900

    feat: 비밀번호 재설정 api 추가

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,10 +1,11 @@
 import { JoinProps } from "../pages/Join";
 import { LoginProps } from "../pages/Login";
 import { httpClient } from "./http";
+import axios, { AxiosError } from 'axios';
 
 // 회원가입
 export const join = async (data: JoinProps) => {
-  const response = await httpClient.post("/join", data);
+  const response = await httpClient.post("/users/join", data);
 
   return response.data;
 };
@@ -15,7 +16,54 @@ interface LoginResponse {
 }
 
 export const login = async (data: LoginProps) => {
-  const response = await httpClient.post<LoginResponse>("/login", data);
+  const response = await httpClient.post<LoginResponse>("/users/login", data);
 
+  return response.data;
+};
+
+
+interface ResetPasswordProps {
+  currentPassword: string;
+  password: string;
+  passwordConfirm: string;
+}
+
+export const resetPassword = async (data: ResetPasswordProps) => {
+  try {
+    console.log('ResetPassword request data:', data); // 추가된 로그
+    const response = await httpClient.put('/users/password', data);
+    console.log('ResetPassword response:', response.data); // 추가된 로그
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error('Axios error resetting password:', error.response?.data || error.message);
+      if (error.response?.data) {
+        console.error('Server error details:', error.response.data);
+      }
+      throw error.response?.data || error.message;
+    } else {
+      console.error('Unknown error resetting password:', error);
+      throw new Error('Unknown error occurred');
+    }
+  }
+};
+
+interface CheckCurrentPasswordResponse {
+  success: boolean;
+}
+
+// 현재 비밀번호 확인
+export const checkCurrentPassword = async (password: string): Promise<CheckCurrentPasswordResponse> => {
+  const response = await httpClient.post<CheckCurrentPasswordResponse>(`/users/action/is-current-password`, { password });
+  return response.data;
+};
+
+interface CheckAvailablePasswordResponse {
+  success: boolean;
+}
+
+// 사용 가능한 비밀번호 확인
+export const checkAvailablePassword = async (password: string, newPassword: string): Promise<CheckAvailablePasswordResponse> => {
+  const response = await httpClient.post<CheckAvailablePasswordResponse>(`/users/action/is-available-password`, { password, newPassword });
   return response.data;
 };
