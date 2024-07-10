32e13c438ec48bdb8181600fd160ea6f7263cff7
gahyeon <117976216+gahyeon11@users.noreply.github.com>
2024-07-10 22:40:44 +0900

    feat: logout api 연결

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,10 +1,11 @@
 import { JoinProps } from "../pages/Join";
 import { LoginProps } from "../pages/Login";
 import { httpClient } from "./http";
+import axios from 'axios';
 
 // 회원가입
 export const join = async (data: JoinProps) => {
-  const response = await httpClient.post("/join", data);
+  const response = await httpClient.post("/users/join", data);
 
   return response.data;
 };
@@ -15,7 +16,16 @@ interface LoginResponse {
 }
 
 export const login = async (data: LoginProps) => {
-  const response = await httpClient.post<LoginResponse>("/login", data);
+  const response = await httpClient.post<LoginResponse>("/users/login", data);
 
   return response.data;
+};
+
+// 로그아웃
+export const logout = async () => {
+  try {
+    await httpClient.post('/users/logout', {});
+  } catch (error) {
+    console.error('로그아웃에 실패하였습니다.', error);
+  }
 };
