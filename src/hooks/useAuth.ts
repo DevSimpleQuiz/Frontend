3c69ea2ae8c2ccf34fe7b6598ba9584369e48fbd
gahyeon <117976216+gahyeon11@users.noreply.github.com>
2024-07-11 10:07:12 +0900

    fix: 401 핸들러 추가, prop명 변경

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,14 +1,26 @@
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useAuthStore } from "../store/authStore";
 import { LoginProps } from "../pages/Login";
-import { checkAvailablePassword, checkCurrentPassword, join, login, resetPassword } from "../api/auth.api";
+import {
+  checkAvailablePassword,
+  checkCurrentPassword,
+  join,
+  login,
+  logout,
+  resetPassword,
+} from "../api/auth.api";
 import { JoinProps } from "../pages/Join";
-import { AxiosError } from 'axios';
-import { ResetProps } from "../pages/ResetPassword";
 
 export const useAuth = () => {
   const navigation = useNavigate();
   const { storeLogin, storeLogout } = useAuthStore();
+
+  const handle401Error = () => {
+    window.alert("토큰이 만료되었습니다. 다시 로그인 해주세요.");
+    storeLogout();
+    navigation("/login");
+  };
 
   const userLogin = (data: LoginProps) => {
     login(data).then(
@@ -31,40 +43,41 @@ export const useAuth = () => {
     });
   };
 
-
   const userLogout = async () => {
     try {
       await logout();
       storeLogout();
-      navigation('/login');
+      navigation("/login");
     } catch (err) {
-      console.error('Error logging out:', err);
+      console.error("Error logging out:", err);
     }
   };
 
-  return {
-    userLogin,
-    userJoin,
-    userLogout
-  };
-  const userResetPassword = (data: ResetProps) => {
-    resetPassword(data).then(
-      () => {
-        window.alert("비밀번호가 재설정되었습니다.");
-        storeLogout();
-        navigation("/login");
-      },
-      (error) => {
-        if (error instanceof AxiosError && error.response?.status === 401) {
-          window.alert("인증이 필요합니다. 다시 로그인 해주세요.");
-          storeLogout();
-          navigation("/login");
+  const userResetPassword = async (
+    currentPassword: string,
+    password: string
+  ) => {
+    try {
+      await resetPassword(currentPassword, password);
+      window.alert("비밀번호가 재설정되었습니다.");
+      storeLogout();
+      navigation("/login");
+    } catch (error: any) {
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 401) {
+          handle401Error();
+        } else if (error.response) {
+          throw error.response.data.errors
+            .map((err: any) => err.msg)
+            .join("\n");
         } else {
-          window.alert("비밀번호 재설정에 실패하였습니다.");
+          throw "서버 응답이 없습니다. 다시 시도해주세요.";
         }
-        console.error('Error resetting password:', error);
+      } else {
+        console.error(error);
+        throw "비밀번호 재설정에 실패하였습니다.";
       }
-    );
+    }
   };
 
   const userCheckCurrentPassword = async (password: string) => {
@@ -72,37 +85,36 @@ export const useAuth = () => {
       const response = await checkCurrentPassword(password);
       return response.success;
     } catch (error) {
-      if (error instanceof AxiosError && error.response?.status === 401) {
-        window.alert("인증이 필요합니다. 다시 로그인 해주세요.");
-        storeLogout();
-        navigation("/login");
+      if (axios.isAxiosError(error) && error.response?.status === 401) {
+        handle401Error();
       } else {
-        console.error("Check current password failed", error);
+        console.error(error);
       }
       return false;
     }
   };
 
-  const userCheckAvailablePassword = async (password: string, newPassword: string) => {
+  const userCheckAvailablePassword = async (
+    password: string,
+    newPassword: string
+  ) => {
     try {
       const response = await checkAvailablePassword(password, newPassword);
       return response.success;
     } catch (error) {
-      if (error instanceof AxiosError && error.response?.status === 401) {
-        window.alert("인증이 필요합니다. 다시 로그인 해주세요.");
-        storeLogout();
-        navigation("/login");
+      if (axios.isAxiosError(error) && error.response?.status === 401) {
+        handle401Error();
       } else {
-        console.error("Check available password failed", error);
+        console.error(error);
       }
       return false;
     }
   };
 
-
   return {
     userLogin,
     userJoin,
+    userLogout,
     userResetPassword,
     userCheckCurrentPassword,
     userCheckAvailablePassword,
