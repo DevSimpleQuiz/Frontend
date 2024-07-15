9486abb066b0ddf43f54750b47eed5f76321abbf
“Dami-LEE00” <dxmxxii@gmail.com>
2024-07-15 11:53:53 +0900

    feat : 랭크 데이터 타입 추가

--- src/models/rank.model.ts
+++ src/models/rank.model.ts
@@ -1,5 +1,21 @@
 export interface Rank {
   id: string;
+  rank?: number;
+  totalQuizScore?: number;
+  totalQuizCount?: number;
+  totalSolvedQuizCount?: number;
+}
+
+export interface TopRank {
+  topRankers: Ranker[];
+}
+
+export interface NearRank {
+  nearRankers: Ranker[];
+}
+
+export interface Ranker {
+  id: string;
   rank: number;
-  score: number;
+  totalScore: number;
 }
