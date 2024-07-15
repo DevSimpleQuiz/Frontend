29181cbca1eade386a8dff4d4a3dbc0af7de1311
yerin <aer561@naver.com>
2024-07-15 21:48:02 +0900

    style : 들여쓰기 2칸으로 수정

--- src/api/quiz.api.ts
+++ src/api/quiz.api.ts
@@ -2,18 +2,16 @@ import { QuizItem } from "../models/quiz.model";
 import { httpClient } from "./http";
 
 interface FetchQuizzesResponse {
-    quizzes: QuizItem[];
+  quizzes: QuizItem[];
 }
 
 export const fetchQuizzes = async () => {
-    try{
-        const response = await httpClient.get<FetchQuizzesResponse>("/quiz");
-        return response.data;
-    }catch (error) {
-        return {
-            quizzes: [],
-            
-        };
-    }
-    
+  try{
+    const response = await httpClient.get<FetchQuizzesResponse>("/quiz");
+    return response.data;
+  }catch (error) {
+    return {
+      quizzes: [],
+    };
+  }    
 };
--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -4,25 +4,23 @@ import { myPage } from "../models/myPage.model";
 import { useNavigate } from "react-router-dom";
 import { useAuthStore } from "../store/authStore";
 import { logout } from "../api/auth.api";
-import { useAuth } from "./useAuth";
 
 export const useUsers = () => {
-    const navigate = useNavigate();
-    const { userLogout } = useAuth();
-    const { storeLogout } = useAuthStore();
-    const [users, setUsers] = useState<myPage>();
-        useEffect(()=>{
-            fetchUsers().then(
-                (user) =>{
-                    if(user){
-                        setUsers(user);
-                    }
-                    else{
-                        userLogout();
-                        navigate("/users/login");
-                    }
-                }
-            );
-        },[]);
-    return {users};
+  const navigate = useNavigate();
+  const { storeLogout } = useAuthStore();
+  const [users, setUsers] = useState<myPage>();
+  useEffect(()=>{
+    fetchUsers().then(
+      (user) =>{
+        if(user){
+          setUsers(user);
+        }else{
+          logout();
+          storeLogout();
+          navigate("/users/login");
+        }
+      }
+    );
+  },[]);
+return {users};
 };
