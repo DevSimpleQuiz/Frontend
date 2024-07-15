500459b9f2267b94e850f931749e0852bded9a5f
“Dami-LEE00” <dxmxxii@gmail.com>
2024-07-15 11:58:58 +0900

    feat : 사용자, 상위 랭킹, 근처 랭킹 데이터를 비동기적으로 가져오는 기능

---
+++ src/hooks/useRank.ts
@@ -0,0 +1,49 @@
+// import { useEffect, useState } from "react";
+// import { Rank } from "../models/rank.model";
+// import { fetchRank } from "../api/rank.api";
+
+// export const useRank = () => {
+//   const [userRank, setUserRank] = useState<Rank>();
+
+//   useEffect(() => {
+//     fetchRank().then((data) => {
+//       setUserRank(data);
+//     })
+//   }, []);
+  
+//   console.log(userRank);
+
+//   return { userRank };
+// };
+
+
+// useRank.ts
+
+import { useEffect, useState } from "react";
+import { NearRank, Rank, TopRank } from "../models/rank.model";
+import { fetchNearRank, fetchRank, fetchTopRank } from "../api/rank.api";
+
+export const useRank = () => {
+  const [userRank, setUserRank] = useState<Rank | null>(null);
+  const [topRank, setTopRank] = useState<TopRank | null>(null);
+  const [nearRank, setNearRank] = useState<NearRank | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userRankData = await fetchRank();
+        const topRankData = await fetchTopRank();
+        const nearRankData = await fetchNearRank();
+        setUserRank(userRankData);
+        setTopRank(topRankData);
+        setNearRank(nearRankData);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { userRank, topRank, nearRank };
+};
