5e42429fbfa0816d6f195207b6e35998fbb4f697
“Dami-LEE00” <dxmxxii@gmail.com>
2024-09-09 04:00:39 +0900

    feat : 랭킹 테이블 분리 및 로그인 이동 url 수정 #51

--- src/pages/Rank.tsx
+++ src/pages/Rank.tsx
@@ -2,9 +2,17 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { useRank } from '../hooks/useRank';
 import UnAuthorizedRank from '../components/common/rank/UnAuthorizedRank';
+import { allRankersDummy } from '../constants/allRankersDummy';
+import RankTable from '../components/common/rank/RankTable';
 
 const Rank = () => {
   const { userRank } = useRank();
+
+  const data = allRankersDummy?.allRankers.map(ranker => ({
+    rank: ranker.rank,
+    id: ranker.id,
+    score: `${ranker.score}점`,
+  })) || [];
 
   if (!userRank) {
     return <UnAuthorizedRank />;
@@ -12,29 +20,44 @@ const Rank = () => {
 
   return (
     <RankWrapper>
-      <h1 className="title">나의 랭킹</h1>
-      <Content>
-        <MyRank>
-          <div className="rank-box">
-            <div className="rank-zone">
-              <h1>RANK</h1>
-              <span>{userRank.rank}</span>
-            </div>
-            <div className="line"></div>
-            <div className="go-page">
-              <h2>잘 하고 있어요!</h2>
-              <Link to='/'>메인 화면으로 가기</Link>
-              <Link to='/mypage'>마이 페이지로 가기</Link>
-            </div>
-          </div>
-          <div className="score-box">
-            <h2>나의 점수는 <span className="blue">{userRank.totalQuizScore}</span>점</h2>
-            <h3>
-              <span className="blue">{userRank.totalQuizCount}</span>문제 중 <span className="blue">{userRank.totalSolvedQuizCount}</span>문제를 맞췄네요!
-            </h3>
-          </div>
-        </MyRank>
-      </Content>
+      <section>
+        <Title>나의 랭킹</Title>
+        <Content>
+          <MyRank>
+            <RankBox className="rank-box">
+              <div className="rank-zone">
+                <h1>RANK</h1>
+                <span>{userRank.rank}</span>
+              </div>
+              <div className="line"></div>
+              <div className="go-page">
+                <h2>잘 하고 있어요!</h2>
+                <Link to='/'>메인 화면으로 가기</Link>
+                <Link to='/mypage'>마이 페이지로 가기</Link>
+              </div>
+            </RankBox>
+            <ScoreBox className="score-box">
+              <h2>
+                나의 점수는&nbsp;
+                <span className="blue">{userRank.totalQuizScore}</span>점
+              </h2>
+              <h3>
+                <span className="blue">{userRank.totalQuizCount}</span>문제 중&nbsp;
+                <span className="blue">{userRank.totalSolvedQuizCount}</span>문제를 맞췄네요!
+              </h3>
+            </ScoreBox>
+          </MyRank>
+        </Content>
+      </section>
+      <section>
+        <Title>전체 랭킹</Title>
+        <Content>
+          <RankTable
+            data={data}
+            highlightId={userRank.id}
+          />
+        </Content>
+      </section>
     </RankWrapper>
   );
 };
@@ -42,14 +65,16 @@ const Rank = () => {
 export const RankWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 14px;
-  padding: 40px 0;
+  gap: 64px;
+  padding: 40px 0 80px 0;
   color: ${({ theme }) => theme.color.primary};
+`;
 
-  .title {
-    font-size: ${({ theme }) => theme.heading.title2};
-    font-weight: 600;
-  }
+export const Title = styled.h1`
+  font-size: ${({ theme }) => theme.heading.title2};
+  font-weight: 600;
+  margin-bottom: 8px;
+  padding: 4px 0;
 `;
 
 export const Content = styled.div`
@@ -74,76 +99,49 @@ export const MyRank = styled.div`
     border: 1px solid ${({ theme }) => theme.color.grey3};
     border-radius: 8px;
   }
+`;
 
-  .rank-box {
+export const RankBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  padding: 14px 0;
+
+  .rank-zone {
+    flex: 1;
     display: flex;
-    flex-direction: row;
-    justify-content: space-around;
+    flex-direction: column;
+    justify-content: center;
     align-items: center;
-    padding: 14px 0;
+    gap: 4px;
 
-    .rank-zone {
-      flex: 1;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      gap: 4px;
-
-      h1 {
-        font-size: ${({ theme }) => theme.heading.title2};
-        font-weight: 800;
-      }
-
-      span {
-        display: inline-block;
-        width: 100px;
-        height: 100px;
-        border-radius: 50%;
-        background-color: ${({ theme }) => theme.color.grey5};
-        font-size: ${({ theme }) => theme.heading.title1};
-        font-weight: 800;
-        line-height: 100px;
-        text-align: center;
-      }
+    h1 {
+      font-size: ${({ theme }) => theme.heading.title2};
+      font-weight: 800;
     }
 
-    .line {
-      width: 1px;
-      height: 100%;
-      background-color: ${({ theme }) => theme.color.grey3};
-    }
-
-    .go-page {
-      flex: 1;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      gap: 8px;
-
-      h2 {
-        margin-bottom: 4px;
-        font-size: ${({ theme }) => theme.heading.title3};
-        font-weight: 600;
-      }
-
-      a {
-        padding: 2px 8px;
-        color: ${({ theme }) => theme.color.primary};
-        background-color: ${({ theme }) => theme.color.grey5};
-        border-radius: 8px;
-        transition: all linear 0.1s;
-
-        &:hover {
-          background-color: ${({ theme }) => theme.color.grey4};
-          transition: all linear 0.1s;
-        }
-      }
+    span {
+      display: inline-block;
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+      background-color: ${({ theme }) => theme.color.grey5};
+      font-size: ${({ theme }) => theme.heading.title1};
+      font-weight: 800;
+      line-height: 100px;
+      text-align: center;
     }
   }
 
-  .score-box {
+  .line {
+    width: 1px;
+    height: 100%;
+    background-color: ${({ theme }) => theme.color.grey3};
+  }
+
+  .go-page {
+    flex: 1;
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -151,23 +149,50 @@ export const MyRank = styled.div`
     gap: 8px;
 
     h2 {
+      margin-bottom: 4px;
       font-size: ${({ theme }) => theme.heading.title3};
       font-weight: 600;
     }
 
-    h3 {
-      font-size: ${({ theme }) => theme.heading.title4};
-      font-weight: 400;
-    }
-
     a {
+      padding: 2px 8px;
       color: ${({ theme }) => theme.color.primary};
-      text-decoration: underline;
-    }
+      background-color: ${({ theme }) => theme.color.grey5};
+      border-radius: 8px;
+      transition: all linear 0.1s;
 
-    .blue {
-      color: ${({ theme }) => theme.color.blue};
+      &:hover {
+        background-color: ${({ theme }) => theme.color.grey4};
+        transition: all linear 0.1s;
+      }
     }
+  }
+`;
+
+export const ScoreBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+
+  h2 {
+    font-size: ${({ theme }) => theme.heading.title3};
+    font-weight: 600;
+  }
+
+  h3 {
+    font-size: ${({ theme }) => theme.heading.title4};
+    font-weight: 400;
+  }
+
+  a {
+    color: ${({ theme }) => theme.color.primary};
+    text-decoration: underline;
+  }
+
+  .blue {
+    color: ${({ theme }) => theme.color.blue};
   }
 `;
 
